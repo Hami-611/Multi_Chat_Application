95b560ec38ac09040e1a6362a7a9e589f2c67691
Hamika Redrowthu <hamiredro611@gmail.com>
2025-03-17 18:44:46 +0530

    Update serializer.py

--- djchat/server/serializer.py
+++ djchat/server/serializer.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 from .models import Category, Channel, Server
 
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
